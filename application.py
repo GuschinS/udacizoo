4115c218c0b08fe0b7d2e260a4909dd00ed2d86a
GuschinS <guschins@gmail.com>
2020-12-11 11:41:43 +0300

    Update application.py
    edit host and port

--- application.py
+++ application.py
@@ -2,9 +2,9 @@ from os import environ
 from FlaskExercise import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', '104.40.0.120')
+    HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '80'))
+        PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT, debug=True)
