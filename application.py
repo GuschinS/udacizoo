124a6fcc28b655385f666db05dd66b6f49a7c211
GuschinS <guschins@gmail.com>
2020-12-11 11:05:51 +0300

    Update application.py
    edit host

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from os import environ
 from FlaskExercise import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'http://udacizooapp.azurewebsites.net/')
+    HOST = environ.get('SERVER_HOST', '13.66.138.105')
     try:
         PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
